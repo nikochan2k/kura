5acd287ea59edf64f7fc499b9be3c67c409a0790
Yoshihiro Seki <nikochan2k@gmail.com>
2020-01-22 02:49:02 +0900

    remove unnecessary interface

--- src/idb/IdbFileWriter.ts
+++ src/idb/IdbFileWriter.ts
@@ -1,10 +1,8 @@
 import { AbstractFileWriter } from "../AbstractFileWriter";
-import { FileWriter } from "../filewriter";
 import { IdbAccessor } from "./IdbAccessor";
 import { IdbFileEntry } from "./IdbFileEntry";
 
-export class IdbFileWriter extends AbstractFileWriter<IdbAccessor>
-  implements FileWriter {
+export class IdbFileWriter extends AbstractFileWriter<IdbAccessor> {
   constructor(fileEntry: IdbFileEntry, file: File) {
     super(fileEntry, file);
   }
