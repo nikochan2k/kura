a27a8cebef071433134c4e53fdea883a3ca37689
Yoshihiro Seki <nikochan2k@gmail.com>
2021-05-31 18:10:38 +0900

    modify method params

--- src/Transferer.ts
+++ src/Transferer.ts
@@ -2,13 +2,13 @@ import { AbstractAccessor } from "./AbstractAccessor";
 import { FileSystemObject } from "./FileSystemObject";
 
 export class Transferer {
-  constructor(
-    protected fromAccessor: AbstractAccessor,
-    protected toAccessor: AbstractAccessor
-  ) {}
-
-  public async transfer(fromObj: FileSystemObject, toObj: FileSystemObject) {
-    const content = await this.fromAccessor.doReadContent(fromObj.fullPath);
-    await this.toAccessor.doWriteContent(toObj.fullPath, content);
+  public async transfer(
+    fromAccessor: AbstractAccessor,
+    fromObj: FileSystemObject,
+    toAccessor: AbstractAccessor,
+    toObj: FileSystemObject
+  ) {
+    const content = await fromAccessor.doReadContent(fromObj.fullPath);
+    await toAccessor.doWriteContent(toObj.fullPath, content);
   }
 }
