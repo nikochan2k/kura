65c849fd2f89701b92bf5563fdd03a71cc051145
Yoshihiro Seki <nikochan2k@gmail.com>
2021-06-03 23:38:41 +0900

    add Util

---
+++ src/Util.ts
@@ -0,0 +1,12 @@
+export const isBrowser =
+  typeof window !== "undefined" && typeof window.document !== "undefined";
+
+export const isNode =
+  typeof process !== "undefined" &&
+  process.versions != null &&
+  process.versions.node != null;
+
+export const isReactNative =
+  typeof navigator !== "undefined" && navigator.product === "ReactNative";
+
+export const hasBuffer = typeof Buffer === "function";
